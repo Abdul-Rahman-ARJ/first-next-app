44fe6de4f9a2d2032acfae25aa038e427a7e58fa
Abdul Rahman <id16abdulrahman@gmail.com>
2022-09-07 13:51:00 +0530

    blog created

---
+++ pages/api/blog/[bno].js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const [bno] = () => {
+  return (
+    <div>
+      <h1>{nno}</h1>
+    </div>
+  )
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [bno]
