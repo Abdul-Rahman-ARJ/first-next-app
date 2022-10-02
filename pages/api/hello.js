2e31a1d4d00beb549334903e1464e3285ea348bf
Abdul Rahman <id16abdulrahman@gmail.com>
2022-10-02 22:24:39 +0530

    next api created

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === 'POST') {
+    // Process a POST request
+    let data = req.body;
+    // console.log(typeof data)
+    res.status(200).json(data)
+  } else {
+    res.status(200).json({ type: req.method })
+    // Handle any other HTTP method
+  }
 }
+
